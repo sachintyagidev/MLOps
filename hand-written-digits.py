e50a58250045a0a1a981d23e15cfac6ad55b2c0f
Sachin Tyagi <sachin.tyagi.email@gmail.com>
2021-08-26 12:52:56 +0530

    rel code change

--- hand-written-digits.py
+++ hand-written-digits.py
@@ -5,6 +5,7 @@ from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 
 digits = datasets.load_digits()
+
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
 for ax, image, label in zip(axes, digits.images, digits.target):
     ax.set_axis_off()
