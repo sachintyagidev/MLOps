226aa36d79f22e15ed1fa82b35f104028112f8b1
Sachin Tyagi <sachin.tyagi.email@gmail.com>
2021-10-08 13:24:44 +0530

    dataset validation split

--- README.md
+++ README.md
@@ -5,8 +5,4 @@ Command to execute from root of repo:
 
 pytest
 
-## Added a test case to check if model is successfully getting created or not?
-### def test_model_writing()
-
-## Added a test case to check fitting on training -- litmus test.
-### def test_small_data_overfit_checking()
+## test cases for the create_split function
--- test/test_example.py
+++ test/test_example.py
@@ -2,6 +2,55 @@ import math
 from ..mlops import digits
 import os
 
+def test_create_split_100():
+    '''
+    generate data via preprocess function
+    '''
+    dateSetSize = 100
+    testSplit = 0.20
+    valSplit = 0.10
+
+    expectedTestSize = round(dateSetSize * testSplit)
+    expectedValSize = round(dateSetSize * valSplit)
+
+    data, target = digits.preprocess(dateSetSize)
+    
+    X_train_s, X_test_s, y_train_s, y_test_s, X_val_s, y_val_s = digits.create_splits(data, target, testSplit, valSplit)
+
+    trianSize = len(X_train_s)
+    testSize = len(X_test_s)
+    valSize = len(X_val_s)
+
+    assert trianSize + testSize + valSize == dateSetSize
+    assert testSize == expectedTestSize
+    assert valSize == expectedValSize
+    assert trianSize == dateSetSize - (expectedTestSize + expectedValSize)
+
+def test_create_split_9():
+    '''
+    generate data via preprocess function
+    '''
+    dateSetSize = 9
+    testSplit = 0.20
+    valSplit = 0.10
+
+    expectedTestSize = round(dateSetSize * testSplit)
+    expectedValSize = round(dateSetSize * valSplit)
+
+    data, target = digits.preprocess(dateSetSize)
+    
+    X_train_s, X_test_s, y_train_s, y_test_s, X_val_s, y_val_s = digits.create_splits(data, target, testSplit, valSplit)
+
+    trianSize = len(X_train_s)
+    testSize = len(X_test_s)
+    valSize = len(X_val_s)
+
+    assert trianSize + testSize + valSize == dateSetSize
+    assert testSize == expectedTestSize
+    assert valSize == expectedValSize
+    assert trianSize == dateSetSize - (expectedTestSize + expectedValSize)
+    
+
 def test_model_writing():
     '''
     generate data via preprocess function
