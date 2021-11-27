858f8b7848bdf2026cd190f05f245877fcc69409
Sachin Tyagi <sachin.tyagi.email@gmail.com>
2021-11-27 23:54:58 +0530

    ass 11

--- README.md
+++ README.md
@@ -1,62 +1,6 @@
 # MLOps
-## EXAM
+## Assignment 11
 
 ### Image build command 
-sudo docker build -t exam . -f docker/Dockerfile
 
-### Container RUN Command
-sudo docker run -d --network=host --name exam -v '/media/sachin/DATA1/IITJ/Sem IV/MLOps/Task/MLOps/modelExam':/code/modelExam exam:latest
-
-### Model Hyperparameter command
-
-python mlops/digits.py
-
-#### The best Gama is : 0.001 and 0.01 is overfitting that's why also perform bad on Test and validation
-
-## Hyperparameter : 0.0001
-
-MPL Result : Train : 1.0 Test : 0.9555555555555556 Validation : 0.9666666666666667
-
-Run 1, Train : 0.9777247414478918, Test : 0.9592592592592593, Validation : 0.9666666666666667
-
-MPL Result : Train : 1.0 Test : 0.9740740740740741 Validation : 0.9777777777777777
-
-Run 2, Train : 0.9761336515513126, Test : 0.9777777777777777, Validation : 0.9666666666666667
-
-MPL Result : Train : 1.0 Test : 0.9703703703703703 Validation : 0.9629629629629629
-
-Run 3, Train : 0.9785202863961814, Test : 0.9592592592592593, Validation : 0.9777777777777777
-
-Mean :Train : 0.9774595597984619, Test : 0.9654320987654321, Validation : 0.9703703703703703
-
-## Hyperparameter : 0.001
-
-MPL Result : Train : 1.0 Test : 0.9592592592592593 Validation : 0.9888888888888889
-
-Run 1, Train : 0.9992044550517104, Test : 0.9888888888888889, Validation : 0.9962962962962963
-
-MPL Result : Train : 1.0 Test : 0.9666666666666667 Validation : 0.9555555555555556
-
-Run 2, Train : 0.9984089101034208, Test : 0.9814814814814815, Validation : 0.9740740740740741
-
-MPL Result : Train : 1.0 Test : 0.9666666666666667 Validation : 0.9777777777777777
-
-Run 3, Train : 1.0, Test : 0.9888888888888889, Validation : 0.9888888888888889
-
-Mean :Train : 0.9992044550517104, Test : 0.9864197530864198, Validation : 0.9864197530864198
-
-## Hyperparameter : 0.01
-
-MPL Result : Train : 1.0 Test : 0.9592592592592593 Validation : 0.9629629629629629
-
-Run 1, Train : 1.0, Test : 0.6444444444444445, Validation : 0.7407407407407407
-
-MPL Result : Train : 1.0 Test : 0.9740740740740741 Validation : 0.9888888888888889
-
-Run 2, Train : 1.0, Test : 0.7814814814814814, Validation : 0.7148148148148148
-
-MPL Result : Train : 1.0 Test : 0.9925925925925926 Validation : 0.9666666666666667
-
-Run 3, Train : 1.0, Test : 0.8185185185185185, Validation : 0.8296296296296296
-
-Mean :Train : 1.0, Test : 0.7481481481481481, Validation : 0.7617283950617284
+![alt text](mlops/images/assignment11.png?raw=true)
--- mlops/digits.py
+++ mlops/digits.py
@@ -12,6 +12,9 @@ from enum import Enum
 import base64
 from statistics import mean
 from sklearn.neural_network import MLPClassifier
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 class Classifier(Enum):
     SVM = 1
@@ -25,10 +28,12 @@ target = digits.target
 n_samples = len(images)
 data = images.reshape((n_samples, -1))
 
-def preprocess(size = 0):
+def preprocess(percent = 0):
     digits = datasets.load_digits()
+    totalImage = len(digits.images)
 
-    if size > 0:
+    if percent > 0:
+        size = int(totalImage * percent / 100)
         images = digits.images[0:size]
         target = digits.target[0:size]
     else:
@@ -37,17 +42,12 @@ def preprocess(size = 0):
 
     n_samples = len(images)
     data = images.reshape((n_samples, -1))
-    #print(n_samples)
 
     return data, target
 
 def create_splits(data, target, test_size, val_size):
     X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=test_size)
-
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=(val_size / (1 - test_size)))
-
-    #print('Train size: ' + str(len(X_train)) + ', ' + 'Test size: ' + str(len(X_test)) + ', ' + "Val size: " + str(len(X_val)) + '\n')
-
     return X_train, X_test, y_train, y_test, X_val, y_val
 
 def test(clf, X_val, y_val):
@@ -57,60 +57,98 @@ def test(clf, X_val, y_val):
 
     return acc, f1
 
-result = {}
+macroF1 = []
+bestModel = []
+filenameGama = 'modelAss11/model_{}_{}.sav'
+testSplit = 0.10
+valSplit = 0.10
 
-def trainExam(hyperParameter, X_train, y_train, X_val, y_val, X_test_s, y_test_s, clfType = Classifier.SVM):
+def train(hyperParameter, datasetSize, X_train, y_train, X_val, y_val, X_test_s, y_test_s, clfType = Classifier.SVM):
     clf = None
     if clfType == Classifier.SVM:
         clf = svm.SVC(gamma=hyperParameter)
     elif clfType == Classifier.DecisionTree:
         clf = tree.DecisionTreeClassifier(max_depth=hyperParameter)
 
-    clfMLP = MLPClassifier(random_state=1, max_iter=300).fit(X_train, y_train)
-    mlpTest = clfMLP.score(X_test_s, y_test_s)
-    mlpTrain = clfMLP.score(X_train, y_train)
-    mlpVal = clfMLP.score(X_val, y_val)
-
-    print('MPL Result : Train : ' + str(mlpTrain) + ' Test : ' + str(mlpTest) + ' Validation : ' + str(mlpVal))
-
-    filenameGama = 'modelExam/model_{}.sav'
-
     clf.fit(X_train, y_train)
     val_acc, val_f1 = test(clf, X_val, y_val)
     train_acc, train_f1 = test(clf, X_train, y_train)
     test_acc, test_f1 = test(clf, X_test_s, y_test_s)
     
-    pickle.dump(clf, open(filenameGama.format(hyperParameter), 'wb'))
+    pickle.dump(clf, open(filenameGama.format(datasetSize, hyperParameter), 'wb'))
     return val_acc, val_f1, train_acc, train_f1, test_acc, test_f1
 
-def trainHyperparameter(gama):
-    valAcc = []
-    testAcc = []
-    trainAcc = []
+def trainHyperparameter(datasize):
+    data, target = preprocess(datasize)
 
-    print('Hyperparameter : ' + str(gama))
-    for i in range(0,3):
-        testSplit = 0.15
-        valSplit = 0.15
-        data, target = preprocess()
-            
+    testF1 = []
+    gamaSet = [10 ** exponent for exponent in range(-6, -1)]
+    bestF1 = 0
+    bestGama = None
+    for gama in gamaSet:
         X_train_s, X_test_s, y_train_s, y_test_s, X_val_s, y_val_s = create_splits(data, target, testSplit, valSplit)
 
-        val_acc, val_f1, train_acc, train_f1, test_acc, test_f1 = trainExam(gama, X_train_s, y_train_s, X_val_s, y_val_s, X_test_s, y_test_s)
-        print('Run ' + str(i + 1) + ", Train : " + str(train_acc) + ", Test : " + str(test_acc) + ", Validation : " + str(val_acc))
-        valAcc.append(val_acc)
-        testAcc.append(test_acc)
-        trainAcc.append(train_acc)
+        val_acc, val_f1, train_acc, train_f1, test_acc, test_f1 = train(gama, datasize, X_train_s, y_train_s, X_val_s, y_val_s, X_test_s, y_test_s)
+        if bestF1 < test_f1:
+            bestF1 = test_f1
+            bestGama = gama
+        print('\t Gama : ' + str(gama) + "\t Train : " + str(train_f1) + "\t Test : " + str(test_f1) + "\t Validation : " + str(val_f1))
+        testF1.append(test_f1)
         val_acc, val_f1, train_acc, train_f1, test_acc, test_f1 = 0,0,0,0,0,0
+    
+    print('\t Dataset size :' + str(datasize) + '\t macro f1 : ' + str(mean(testF1)))
+    macroF1.append(mean(testF1))
+    bestModel.append(bestGama)
 
-    print('Mean :' + 'Train : ' + str(mean(trainAcc)) + ", Test : " + str(mean(testAcc)) + ", Validation : " + str(mean(valAcc))+ '\n')
+def mainAss11():
 
-def mainExam():
+    for size in range(10, 110, 10):
+        print('Dataset size:' + str(size))
+        trainHyperparameter(size)
+    
+    plt.plot(range(10, 110, 10), macroF1)
+    plt.xlabel("Dataset Size")
+    plt.ylabel("macro f1")
+    plt.show()
 
-    gamaSet = [10 ** exponent for exponent in range(-4, -1)]
-    result['Hyperparameter'] = gamaSet
-    for gama in gamaSet:
-        trainHyperparameter(gama)
+    data, target = preprocess()
+    X_train_s, X_test_s, y_train_s, y_test_s, X_val_s, y_val_s = create_splits(data, target, testSplit, valSplit)
 
+    for size in range(1, 10, 1):
+        idx1 = size
+        size1 = 10*(size+1)
+
+        idx2 = size - 1
+        size2 = size*10
+
+        loadedModel1 = pickle.load(open(filenameGama.format(size1, bestModel[idx1]), 'rb'))
+        loadedModel2 = pickle.load(open(filenameGama.format(size2, bestModel[idx2]), 'rb'))
+
+        acc_1, f1_1 = test(loadedModel1, X_test_s, y_test_s)
+        acc_2, f1_2 = test(loadedModel2, X_test_s, y_test_s)
+
+        predicted1 = loadedModel1.predict(X_test_s)
+        predicted2 = loadedModel2.predict(X_test_s)
+
+        conf1 = confusion_matrix(y_test_s, predicted1)
+        conf2 = confusion_matrix(y_test_s, predicted2)
+
+        print('\nModel :' + str(size1) + ' and ' + str(size2))
+        print('F1 : ' + str(f1_1) + ' and ' + str(f1_2))
+
+        ax1 = sns.heatmap(conf1, annot=True, cmap='Blues')
+        ax1.set_title('Confusion Matrix of model size : {} \n\n'.format(size1));
+        #plt.show()
+
+        ax2 = sns.heatmap(conf2, annot=True, cmap='Blues')
+        ax2.set_title('Confusion Matrix of model size : {} \n\n'.format(size2));
+        plt.show()
+        #print('Confusion Matrix :')
+        #print(conf1)
+        #print('\n')
+        #print(conf2)
+
+        #pickle.dump(clf, open(, 'wb'))
     print('Done')
-mainExam()
+
+mainAss11()
